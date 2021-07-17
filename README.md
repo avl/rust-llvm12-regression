830f0fb77a6b316918a63f3224ee535497a00e9f
Anders Musikka <anders@andersmusikka.se>
2021-07-17 17:52:32 +0200

    Add more analysis

--- README.md
+++ README.md
@@ -4,4 +4,36 @@ This is a minimized test case for a rust-program that worked prior to upgrade to
 
 The program is the result of minimizing a much larger program. The program in its present state doesn't really make sense. But it probably shouldn't crash with a seg fault.
 
+The problem appears to be the glium macro "implement_buffer_content".
+
+It does the following to calculate the size of a DST like this:
+
+```rust
+struct Data {
+    data: [u32]
+}
+
+implement_buffer_content!(Data);
+```
+
+See docs:
+https://docs.rs/glium/0.30.1/glium/macro.implement_buffer_content.html
+
+Unfortunately, it then tries to figure out the minimum size and step by doing this:
+
+```rust
+use std::mem;
+
+let fake_ptr: &$struct_name = unsafe { mem::transmute((0usize, 0usize)) };
+let min_size = mem::size_of_val(fake_ptr);
+
+let fake_ptr: &$struct_name = unsafe { mem::transmute((0usize, 1usize)) };
+let step = mem::size_of_val(fake_ptr) - min_size;
+
+size > min_size && (size - min_size) % step == 0
+```
+
+I believe the transmutes constitute undefined behaviour. Apparently, before LLVM 12
+it worked. How can we salvage this?
+
 
--- src/main.rs
+++ src/main.rs
@@ -42,7 +42,64 @@ struct ObservedHeight {
     observed_heights: [f32],
 }
 
-implement_buffer_content!(ObservedHeight);
+// implement_buffer_content!(ObservedHeight);
+// The above macro doesn't implement glium::buffer::Content
+// correctly. Here is a partial implementation (just enough to trigger the problem)
+unsafe impl glium::buffer::Content for ObservedHeight {
+    type Owned = Box<ObservedHeight>;
+
+    #[inline]
+    fn read<F, E>(size: usize, _: F) -> Result<Self::Owned, E> where F: FnOnce(&mut Self) -> Result<(), E> {
+        todo!()
+    }
+
+    #[inline]
+    fn get_elements_size() -> usize {
+        use std::mem;
+        let fake_ptr: &ObservedHeight = unsafe { mem::transmute((0usize, 0usize)) };
+        let size = mem::size_of_val(fake_ptr);
+        println!("Size of val: {}", size);
+        size
+    }
+
+    #[inline]
+    fn to_void_ptr(&self) -> *const () {
+        println!("to_void_ptr");
+        &self as *const _ as *const ()
+    }
+
+    #[inline]
+    fn ref_from_ptr<'a>(ptr: *mut (), size: usize) -> Option<*mut Self> {
+        todo!()
+    }
+
+    #[inline]
+    fn is_size_suitable(size: usize) -> bool {
+        println!("is_size_suitable");
+        use std::mem;
+
+        let fake_ptr: &ObservedHeight = unsafe { mem::transmute((0usize, 0usize)) };
+        // The idea here is that min_size should become the smallest possible size
+        // of the runtime sized object. In this example-program that size is 0
+        // (since the array of ObservedHeight can be 0-length).
+        let min_size = mem::size_of_val(fake_ptr);
+
+        let fake_ptr: &ObservedHeight = unsafe { mem::transmute((0usize, 1usize)) };
+        // The idea here is that step should become the size of each array element
+        // in the final array of the dynamically sized object. In this case 4,
+        // because the array in ObservedHeight is of f32.
+        let step = mem::size_of_val(fake_ptr) - min_size;
+
+        // The calculation of min_size and step here works in debug mode,
+        // but not in release.
+
+        println!("Min size: {}, step: {} ", min_size, step);
+        size > min_size && (size - min_size) % step == 0
+
+    }
+}
+
+
 
 pub fn main() {
     let mut watcher = TkWatcher::start();
@@ -50,8 +107,13 @@ pub fn main() {
     let events_loop = glutin::event_loop::EventLoop::new();
     let window = glutin::window::WindowBuilder::new();
     let context = glutin::ContextBuilder::new();
+    println!("About to create display");
     let display: glium::Display = glium::Display::new(window, context, &events_loop).unwrap();
     let s = 512 * 512 * 4;
-    let obsbuffer : glium::uniforms::UniformBuffer<ObservedHeight> = glium::uniforms::UniformBuffer::empty_unsized_persistent(&display, s as usize).unwrap();
+    println!("About to create uniform buffer");
+    let obsbuffer : Result<glium::uniforms::UniformBuffer<ObservedHeight>,_> = glium::uniforms::UniformBuffer::empty_unsized_persistent(&display, s as usize);
+    println!("empty_unsized_persistent returned. Calling unwrap");
+    obsbuffer.unwrap();
+    println!("Done");
 
 }
